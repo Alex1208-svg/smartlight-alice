64222bc794afb0448d243f6d8eefec4251bfd312
Alex1208-svg <51704164+Alex1208-svg@users.noreply.github.com>
2026-07-24 19:54:43 +0300

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
-MQTT_HOST = "eff66a679ce34bd480bbf9946f2f2510.s1.eu.hivemq.cloud"
-MQTT_PORT = 8883
+import os
 
-MQTT_USER = "smartlight"
-MQTT_PASSWORD = "Alex1208"
-
-MQTT_TOPIC = "smartlight/lamp"
+MQTT_HOST = os.getenv("MQTT_HOST")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 8883))
+MQTT_USER = os.getenv("MQTT_USER")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
+MQTT_TOPIC = os.getenv("MQTT_TOPIC")
