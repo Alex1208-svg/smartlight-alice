770cb740359d7e791ddfcc629ac6bd0ab6e54d05
Alex1208-svg <51704164+Alex1208-svg@users.noreply.github.com>
2026-07-25 12:04:37 +0300

    Update oauth.py

--- server/oauth.py
+++ server/oauth.py
@@ -1,1 +1,12 @@
+from flask import redirect
 
+def authorize():
+    return redirect("/success")
+
+def token():
+    return {
+        "access_token": "smartlight_token",
+        "token_type": "bearer",
+        "expires_in": 31536000,
+        "refresh_token": "refresh_token"
+    }
