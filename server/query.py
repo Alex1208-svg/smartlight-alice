a9d4949da84d8b49dc018a024124d3474c38d79b
Alex1208-svg <51704164+Alex1208-svg@users.noreply.github.com>
2026-07-25 18:11:50 +0300

    Update query.py

--- server/query.py
+++ server/query.py
@@ -1,20 +1,73 @@
 from flask import jsonify
-import state
 
 def query():
 
     return jsonify({
-        "request_id": "1",
-        "payload": {
-            "devices": [{
-                "id": "smartlight",
-                "capabilities": [{
-                    "type": "devices.capabilities.on_off",
-                    "state": {
-                        "instance": "on",
-                        "value": state.state
-                    }
-                }]
-            }]
+
+        "request_id":"1",
+
+        "payload":{
+
+            "devices":[
+
+                {
+
+                    "id":"smartlight",
+
+                    "capabilities":[
+
+                        {
+
+                            "type":"devices.capabilities.on_off",
+
+                            "state":{
+
+                                "instance":"on",
+
+                                "value":True
+
+                            }
+
+                        },
+
+                        {
+
+                            "type":"devices.capabilities.range",
+
+                            "state":{
+
+                                "instance":"brightness",
+
+                                "value":100
+
+                            }
+
+                        },
+
+                        {
+
+                            "type":"devices.capabilities.color_setting",
+
+                            "state":{
+
+                                "instance":"rgb",
+
+                                "value":{
+
+                                    "rgb":16777215
+
+                                }
+
+                            }
+
+                        }
+
+                    ]
+
+                }
+
+            ]
+
         }
+
     })
