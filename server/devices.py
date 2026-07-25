2678c205f3609e334e8bdd5c205baafcaeb4e3db
Alex1208-svg <51704164+Alex1208-svg@users.noreply.github.com>
2026-07-25 18:09:10 +0300

    Update devices.py

--- server/devices.py
+++ server/devices.py
@@ -1,24 +1,79 @@
 from flask import jsonify
 
-def get_devices():
+def devices():
 
     return jsonify({
-        "request_id": "1",
-        "payload": {
-            "user_id": "user1",
-            "devices": [
-                {
-                    "id": "smartlight",
-                    "name": "Лента",
-                    "type": "devices.types.light",
 
-                    "capabilities": [
+        "request_id":"1",
+
+        "payload":{
+
+            "user_id":"1",
+
+            "devices":[
+
+                {
+
+                    "id":"smartlight",
+
+                    "name":"Лента",
+
+                    "type":"devices.types.light",
+
+                    "capabilities":[
+
                         {
-                            "type": "devices.capabilities.on_off",
-                            "retrievable": True
+
+                            "type":"devices.capabilities.on_off"
+
+                        },
+
+                        {
+
+                            "type":"devices.capabilities.range",
+
+                            "retrievable":True,
+
+                            "parameters":{
+
+                                "instance":"brightness",
+
+                                "unit":"unit.percent",
+
+                                "range":{
+
+                                    "min":1,
+
+                                    "max":100,
+
+                                    "precision":1
+
+                                }
+
+                            }
+
+                        },
+
+                        {
+
+                            "type":"devices.capabilities.color_setting",
+
+                            "retrievable":True,
+
+                            "parameters":{
+
+                                "color_model":"rgb"
+
+                            }
+
                         }
+
                     ]
+
                 }
+
             ]
+
         }
+
     })
