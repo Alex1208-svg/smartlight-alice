ba55994302e44d526907f8e0eb24b7ec78e560b3
Alex1208-svg <51704164+Alex1208-svg@users.noreply.github.com>
2026-07-25 18:08:29 +0300

    Update actions.py

--- server/actions.py
+++ server/actions.py
@@ -1,33 +1,73 @@
 from flask import request, jsonify
 from mqtt import send
-import state
 
 def action():
+
     data = request.json
 
-    value = data["payload"]["devices"][0]["capabilities"][0]["state"]["value"]
+    print(data)
 
-    state.state = value
+    device = data["payload"]["devices"][0]
+    capability = device["capabilities"][0]
 
-    if value:
-        send("ON")
-    else:
-        send("OFF")
+    state = capability["state"]
+
+    command = {}
+
+    if capability["type"] == "devices.capabilities.on_off":
+
+        command["power"] = state["value"]
+
+    elif capability["type"] == "devices.capabilities.range":
+
+        command["brightness"] = int(state["value"] * 255 / 100)
+
+    elif capability["type"] == "devices.capabilities.color_setting":
+
+        rgb = state["value"]["rgb"]
+
+        command["color"] = "#{:06X}".format(rgb)
+
+    send(command)
 
     return jsonify({
-        "request_id": data["payload"]["action_id"],
+
+        "request_id": data["request_id"],
+
         "payload": {
-            "devices": [{
-                "id": "smartlight",
-                "capabilities": [{
-                    "type": "devices.capabilities.on_off",
-                    "state": {
-                        "instance": "on",
-                        "action_result": {
-                            "status": "DONE"
+
+            "devices": [
+
+                {
+
+                    "id": "smartlight",
+
+                    "capabilities": [
+
+                        {
+
+                            "type": capability["type"],
+
+                            "state": {
+
+                                "instance": state["instance"],
+
+                                "action_result": {
+
+                                    "status": "DONE"
+
+                                }
+
+                            }
+
                         }
-                    }
-                }]
-            }]
+
+                    ]
+
+                }
+
+            ]
+
         }
+
     })
