08c9baecc62515813165158cd15898579c43c5ff
Alex1208-svg <51704164+Alex1208-svg@users.noreply.github.com>
2026-07-26 12:41:52 +0300

    Update mqtt.py

--- server/mqtt.py
+++ server/mqtt.py
@@ -1,24 +1,43 @@
 import ssl
 import json
 import paho.mqtt.publish as publish
+
 from config import *
+
 
 def send(data):
 
-    print("MQTT SEND:", data)
+    payload = json.dumps(data)
 
-    publish.single(
-        MQTT_TOPIC,
-        json.dumps(data),
-        hostname=MQTT_HOST,
-        port=MQTT_PORT,
-        auth={
-            "username": MQTT_USER,
-            "password": MQTT_PASSWORD
-        },
-        tls={
-            "tls_version": ssl.PROTOCOL_TLS
-        }
-    )
+    print("\n=========== MQTT DEBUG ===========")
+    print("HOST     :", MQTT_HOST)
+    print("PORT     :", MQTT_PORT)
+    print("USER     :", MQTT_USER)
+    print("PASSWORD :", MQTT_PASSWORD)
+    print("TOPIC    :", MQTT_TOPIC)
+    print("PAYLOAD  :", payload)
 
-    print("MQTT OK")
+    try:
+
+        publish.single(
+            topic=MQTT_TOPIC,
+            payload=payload,
+            hostname=MQTT_HOST,
+            port=MQTT_PORT,
+            auth={
+                "username": MQTT_USER,
+                "password": MQTT_PASSWORD
+            },
+            tls={
+                "tls_version": ssl.PROTOCOL_TLS
+            }
+        )
+
+        print("PUBLISH OK")
+
+    except Exception as e:
+
+        print("MQTT ERROR")
+        print(e)
+
+    print("==================================\n")
